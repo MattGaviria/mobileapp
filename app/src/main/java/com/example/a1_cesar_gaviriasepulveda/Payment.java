711c1c0ff177254f7af5ad10b33453e6d2e1a429
MattGaviria <cmateogaviria@gmail.com>
2025-10-01 14:45:48 -0400

    Payment.java and DetailActivity.java ready

--- app/src/main/java/com/example/a1_cesar_gaviriasepulveda/Payment.java
+++ app/src/main/java/com/example/a1_cesar_gaviriasepulveda/Payment.java
@@ -1,5 +1,21 @@
 package com.example.a1_cesar_gaviriasepulveda;
 
 public class Payment {
-    public final double no_of_hours,
+    public final double hours, rate, basePay, overtimePay, totalPay, tax;
+
+    public Payment(double hours, double rate, double basePay,
+                   double overtimePay, double totalPay, double tax) {
+        this.hours = hours;
+        this.rate = rate;
+        this.basePay = basePay;
+        this.overtimePay = overtimePay;
+        this.totalPay = totalPay;
+        this.tax = tax;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Hours: %.1f Rate: $%.2f Total: $%.2f Tax: $%.2f",
+                hours, rate, totalPay, tax);
+    }
 }
